47e80f6f2c06efa92b8aa0871c2eef49dc23ea68
Chris Kelly <chris@workinggears.com>
2023-11-19 12:25:11 +1100

    add pane to tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,38 @@
+import { CupertinoPane } from 'cupertino-pane';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { useEffect, useRef } from 'react';
+
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  const panelRef = useRef<HTMLDivElement>(null);
+  const myPane = useRef<CupertinoPane>();
+
+  useEffect(() => {
+    // Uncomment to prevent error
+    // (document.querySelector('ion-app') as any).componentOnReady = () => {};
+
+    myPane.current = new CupertinoPane(panelRef.current!, {
+      parentElement: 'ion-app',
+      initialBreak: 'top',
+      breaks: {
+        top: { enabled: true, height: 410 },
+        middle: { enabled: false },
+        bottom: { enabled: false },
+      },
+      showDraggable: false,
+    });
+
+    return () => {
+      myPane.current?.destroy();
+    };
+  }, []);
+
+  const openRecord = () => {
+    myPane.current?.present({ animate: true });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +46,11 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <button onClick={openRecord}>Open pane</button>
+        <div ref={panelRef}>
+          Hello, world!
+        </div>
       </IonContent>
     </IonPage>
   );
